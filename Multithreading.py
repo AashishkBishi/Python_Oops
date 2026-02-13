22017b9afb9ef5025b34d462f7b1a7877e89bc4f
AashishkBishi <aswinibishi02@gmail.com>
2026-02-13 11:25:17 +0530

    threading
    Fixed indentation and added output comments for clarity.

--- Multithreading.py
+++ Multithreading.py
@@ -12,4 +12,16 @@ t2= threading.Thread(target = sample2)
 print(t1)
 print(t2)
 t1.start()
-t2.start() 
+t2.start()
+
+output: <Thread(Thread-1 (sample1), initial)>
+        <Thread(Thread-2 (sample2), initial)>
+        1
+        10
+        11
+        12
+        13
+        14
+        2
+        3
+        4
